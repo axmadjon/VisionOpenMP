2ab46020808b796f17b4bb1153777afd48c736fd
axmadjon <axmadjon.xamidov@gmail.com>
2020-11-04 11:25:07 +0500

    initial commit

---
+++ VisionOpenMP/VisionOpenMP.h
@@ -0,0 +1,18 @@
+//
+//  VisionOpenMP.h
+//  VisionOpenMP
+//
+//  Created by MacBook Air GWS on 11/4/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VisionOpenMP.
+FOUNDATION_EXPORT double VisionOpenMPVersionNumber;
+
+//! Project version string for VisionOpenMP.
+FOUNDATION_EXPORT const unsigned char VisionOpenMPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisionOpenMP/PublicHeader.h>
+
+
